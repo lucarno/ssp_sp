3948b5878442f9e95f79d76c704b2ade01b8ddbc
Lucas Novaes <lucarno@users.noreply.github.com>
2018-02-15 10:57:40 +0100

    Add files via upload

---
+++ ssp_sp_scrape.R
@@ -0,0 +1,46 @@
+#library(dplyr)
+library(RSelenium)
+
+# one needs to download java development kit, insert the selenium standalone server at the folder of the script. for a nice walkthrough, see https://rpubs.com/grahamplace/rseleniumonmac
+
+#res = rsDriver(port = 4567L, browser = c('chrome'), version = "latest", chromever = "latest",
+#         geckover = "latest", iedrver = NULL, phantomver = "2.1.1",
+#         verbose = TRUE, check = TRUE)
+
+remDr <- remoteDriver(remoteServerAddr = "localhost"
+                      , port = 5556
+                      , browserName = "firefox")
+
+driver<- rsDriver()
+remDr <- driver[["client"]]
+
+buttons = c('cphBody_btnHomicicio','cphBody_btnLatrocinio','cphBody_btnMortePolicial','cphBody_btnMorteSuspeita','cphBody_btnFurtoVeiculo','cphBody_btnRouboVeiculo','cphBody_btnFurtoCelular')
+
+for(k in buttons){
+  for(i in c(3:17)){
+    remDr$navigate("http://www.ssp.sp.gov.br/transparenciassp/Consulta.aspx")
+    categoria <- remDr$findElement(using = 'id', value = k)
+    categoria$sendKeysToElement(list("\uE007"))
+    year <- remDr$findElement(using = 'id', value = paste0("cphBody_lkAno",i,sep=""))
+    year$sendKeysToElement(list("\uE007"))#clicks on year
+    for(j in c(1:12)){
+        month <- remDr$findElement(using = 'id', value = paste0("cphBody_lkMes",j,sep=""))
+        month$sendKeysToElement(list("\uE007"))#clicks on month
+        planilha <- remDr$findElement(using = 'id', value = "cphBody_ExportarBOLink")
+        planilha$clickElement()
+  Sys.sleep(20)
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
