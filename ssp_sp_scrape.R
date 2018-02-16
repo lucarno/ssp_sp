50fc0d6144a4a6fd3146c8169f32aa26593cc165
Lucas Novaes <lucarno@users.noreply.github.com>
2018-02-16 10:00:52 +0100

    Update ssp_sp_scrape.R
    Several changes, bug fixes.

--- ssp_sp_scrape.R
+++ ssp_sp_scrape.R
@@ -1,45 +1,48 @@
-
+rm(list = ls())
+#library(dplyr)
 library(RSelenium)
 
-# You need to download java development kit, and insert the selenium standalone server at the folder of the script. 
-# For a nice walkthrough, see https://rpubs.com/grahamplace/rseleniumonmac.
-# You also need to have installed the Mozilla Firefox web browser.
+# one needs to download java development kit, insert the selenium standalone server at the folder of the script. for a nice walkthrough, see https://rpubs.com/grahamplace/rseleniumonmac
 
-remDr <- remoteDriver(remoteServerAddr = "localhost"
-                      , port = 5556
-                      , browserName = "firefox")
+res = rsDriver(port = 4567L, browser = c('chrome'), version = "latest", chromever = "latest",
+         geckover = "latest", iedrver = NULL, phantomver = "2.1.1",
+         verbose = TRUE, check = TRUE)
 
-driver<- rsDriver()
-remDr <- driver[["client"]]
+#remDr <- remoteDriver(remoteServerAddr = "localhost"
+#                      , port = 5556
+#                      , browserName = "chrome")
 
-buttons = c('cphBody_btnHomicicio','cphBody_btnLatrocinio','cphBody_btnMortePolicial',
-            'cphBody_btnMorteSuspeita','cphBody_btnFurtoVeiculo','cphBody_btnRouboVeiculo','cphBody_btnFurtoCelular')
+#driver<- rsDriver()
+remDr <- res[["client"]]
+
+
+buttons = c('cphBody_btnMortePolicial','cphBody_btnFurtoVeiculo','cphBody_btnRouboVeiculo','cphBody_btnFurtoCelular','cphBody_btnRouboCelular','cphBody_btnIML')
+
+#'cphBody_btnMorteSuspeita' not available (2/15/2)
 
 for(k in buttons){
-  for(i in c(3:17)){
     remDr$navigate("http://www.ssp.sp.gov.br/transparenciassp/Consulta.aspx")
     categoria <- remDr$findElement(using = 'id', value = k)
     categoria$sendKeysToElement(list("\uE007"))
-    year <- remDr$findElement(using = 'id', value = paste0("cphBody_lkAno",i,sep=""))
-    year$sendKeysToElement(list("\uE007"))#clicks on year
-    for(j in c(1:12)){
+    for(i in c(3:17)){
+        possibleError <- tryCatch(
+          remDr$findElement(using = 'id', value = paste0("cphBody_lkAno",i,sep="")),
+          error=function(e) e
+        )
+  
+        if(!inherits(possibleError, "error")){
+          print(paste0('Starting year ',2000 + i,sep=''))
+          year = remDr$findElement(using = 'id', value = paste0("cphBody_lkAno",i,sep=""))
+      year$sendKeysToElement(list("\uE007"))#clicks on year
+      for(j in c(1:12)){
+        print(paste0('Clicking on year ',2000 + i,', month ',j,sep=''))        
         month <- remDr$findElement(using = 'id', value = paste0("cphBody_lkMes",j,sep=""))
         month$sendKeysToElement(list("\uE007"))#clicks on month
         planilha <- remDr$findElement(using = 'id', value = "cphBody_ExportarBOLink")
         planilha$clickElement()
   Sys.sleep(20)
-    }
+      }}
+      else{
+        print(paste0('Some or all files for the year ', 2000+i,' are unavailable.',sep=''))}
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
